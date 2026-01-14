6edd2305fe84c70cb332c3114f504501bae0b32b
dollybenson <113758345+dollybenson@users.noreply.github.com>
2026-01-14 23:00:25 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Points to your app folder
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // <--- This line is critical!
   ],
   theme: {
     extend: {},
